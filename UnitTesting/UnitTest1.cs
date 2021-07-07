aa9e430f54bffa04985719a115ac175db619b0a9
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-07 16:50:55 +0530

    [Afrath]Updated the file with Test case 2

--- UnitTesting/UnitTest1.cs
+++ UnitTesting/UnitTest1.cs
@@ -16,10 +16,18 @@ namespace UnitTesting
         [TestMethod]
         public void TestMethod1()
         {
+            //Test case 1
+            /*
             int num1 = 5, num2 = 3,num3 = 4,expectedoutput =0,actualoutput = 0;
             actualoutput = findThree.FindMaxNumber(num1,num2,num3);
             expectedoutput = num1;
-            Assert.AreEqual(actualoutput,expectedoutput);
+            Assert.AreEqual(actualoutput,expectedoutput);*/
+            int num1 = 3, num2 = 5, num3 = 4, expectedoutput = 0, actualoutput = 0;
+            actualoutput = findThree.FindMaxNumber(num1, num2, num3);
+            expectedoutput = num2;
+            Assert.AreEqual(actualoutput, expectedoutput);
+
+
         }
     }
 
