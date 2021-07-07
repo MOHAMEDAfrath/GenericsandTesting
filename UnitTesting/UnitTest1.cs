42c27a7ba7d5aeb3a455fe150769a1f536acba8c
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-07 18:02:07 +0530

    [Afrath]Updated the file UnitTestCaseT2

--- UnitTesting/UnitTest1.cs
+++ UnitTesting/UnitTest1.cs
@@ -12,41 +12,32 @@ namespace UnitTesting
         {
             findThree = new FindThreeNumbers(); 
         }
-        
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            double num1 = 5.2, num2 = 3.5, num3 = 4.7, expectedoutput = 0, actualoutput = 0;
+            actualoutput = findThree.FindMaxNumber(num1, num2, num3);
+            expectedoutput = num1;
+            Assert.AreEqual(actualoutput, expectedoutput);
+        }
         [TestMethod]
         public void TestMethod1()
         {
-            //Test case 1 with large integer number at number1
-            /*
-            int num1 = 5, num2 = 3,num3 = 4,expectedoutput =0,actualoutput = 0;
-            actualoutput = findThree.FindMaxNumber(num1,num2,num3);
-            expectedoutput = num1;
-            Assert.AreEqual(actualoutput,expectedoutput);
-             //Test case 1 with large integer number at number2
-            int num1 = 3, num2 = 5, num3 = 4, expectedoutput = 0, actualoutput = 0;
-            actualoutput = findThree.FindMaxNumber(num1, num2, num3);
-            expectedoutput = num2;
-            Assert.AreEqual(actualoutput, expectedoutput);
-            //Test case 1 with large integer number at number3
-            int num1 = 3, num2 = 4, num3 = 5, expectedoutput = 0, actualoutput = 0;
-            actualoutput = findThree.FindMaxNumber(num1, num2, num3);
-            expectedoutput = num3;
-            Assert.AreEqual(actualoutput, expectedoutput);*/
-            //Test case 2 with large double number at number1
-            /* 
-             double num1 = 5.2, num2 = 3.5, num3 = 4.7, expectedoutput = 0, actualoutput = 0;
-             actualoutput = findThree.FindMaxNumber(num1, num2, num3);
-             expectedoutput = num1;
-             Assert.AreEqual(actualoutput, expectedoutput);
             //Test case 2 with large double number at number2
             double num1 = 3.5, num2 = 5.2, num3 = 4.7, expectedoutput = 0, actualoutput = 0;
             actualoutput = findThree.FindMaxNumber(num1, num2, num3);
             expectedoutput = num2;
-            Assert.AreEqual(actualoutput, expectedoutput);*/
-            double num1 = 3.5, num2 = 4.7, num3 = 5.2, expectedoutput = 0, actualoutput = 0;
-            actualoutput = findThree.FindMaxNumber(num1, num2, num3);
-            expectedoutput = num3;
             Assert.AreEqual(actualoutput, expectedoutput);
+        }
+            [TestMethod]
+            public void TestMethod3()
+            {
+                double num1 = 3.5, num2 = 4.7, num3 = 5.2, expectedoutput = 0, actualoutput = 0;
+                actualoutput = findThree.FindMaxNumber(num1, num2, num3);
+                expectedoutput = num3;
+                Assert.AreEqual(actualoutput, expectedoutput);
+            }
 
 
 
@@ -56,4 +47,4 @@ namespace UnitTesting
     }
 
     
-}
+
