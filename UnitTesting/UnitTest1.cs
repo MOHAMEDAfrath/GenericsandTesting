01e4d405e3a00f578007d747443a06f4bfe12127
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-07 17:10:34 +0530

    [Afrath]Updated the file with test case to check double

--- UnitTesting/UnitTest1.cs
+++ UnitTesting/UnitTest1.cs
@@ -33,10 +33,23 @@ namespace UnitTesting
             expectedoutput = num3;
             Assert.AreEqual(actualoutput, expectedoutput);*/
             //Test case 2 with large double number at number1
-            double num1 = 5, num2 = 3, num3 = 4, expectedoutput = 0, actualoutput = 0;
+            /* 
+             double num1 = 5.2, num2 = 3.5, num3 = 4.7, expectedoutput = 0, actualoutput = 0;
+             actualoutput = findThree.FindMaxNumber(num1, num2, num3);
+             expectedoutput = num1;
+             Assert.AreEqual(actualoutput, expectedoutput);
+            //Test case 2 with large double number at number2
+            double num1 = 3.5, num2 = 5.2, num3 = 4.7, expectedoutput = 0, actualoutput = 0;
             actualoutput = findThree.FindMaxNumber(num1, num2, num3);
-            expectedoutput = num1;
+            expectedoutput = num2;
+            Assert.AreEqual(actualoutput, expectedoutput);*/
+            double num1 = 3.5, num2 = 4.7, num3 = 5.2, expectedoutput = 0, actualoutput = 0;
+            actualoutput = findThree.FindMaxNumber(num1, num2, num3);
+            expectedoutput = num3;
             Assert.AreEqual(actualoutput, expectedoutput);
+
+
+
 
 
         }
