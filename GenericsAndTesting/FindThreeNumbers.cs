0ac65ab603ab4565da884057206e5a8507129462
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-07 20:35:19 +0530

    [Afrath]Updated the file Generics

--- GenericsAndTesting/FindThreeNumbers.cs
+++ GenericsAndTesting/FindThreeNumbers.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace GenericsAndTesting
 {
-    public class FindThreeNumbers
+    public class FindThreeNumbers<T> where T : IComparable
     {
-        public string FindMaxNumber(string number1, string number2, string number3)
+        public T number1, number2, number3;
+        public FindThreeNumbers(T number1, T number2, T number3)
+        {
+            this.number1 = number1;
+            this.number2 = number2;
+            this.number3 = number3;
+
+        }
+        public T FindMaxNumber(T number1, T number2, T number3)
         {
             if ((number1.CompareTo(number2) > 0) && (number1.CompareTo(number3) > 0))
             {
@@ -23,5 +31,20 @@ namespace GenericsAndTesting
                 return number3;
             }
         }
+        public T sorting(T[] array)
+        {
+            Array.Sort(array);
+            return array[array.Length - 1];
+        }
+        public T Max()
+        {
+            T max = FindMaxNumber(this.number1, this.number2, this.number3);
+            return max;
+   
+        }
+        
+        
+   
+        
     }
 }
