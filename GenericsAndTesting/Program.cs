84df572d45c5e8368f721344d2fc9f6d98e50296
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-07 15:56:02 +0530

    [Afrath] Welcome to generics and testing

---
+++ GenericsAndTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsAndTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics and Testing!");
+        }
+    }
+}
